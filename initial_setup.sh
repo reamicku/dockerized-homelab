146d1d1c708c8066b06b23b55959ba71f14473ac
Michał Kluz <michalk495@wp.pl>
2023-02-07 20:38:10 +0100

    Fix initial setup

--- initial_setup.sh
+++ initial_setup.sh
@@ -7,21 +7,21 @@ export $(grep -v '^#' .env | xargs)
 echo Initial configuration
 
 echo Create directories
-mkdir $DOCKERDIR/shared
+mkdir -p $DOCKERDIR/shared
 
 ## Traefik2
 echo Traefik
 TRAEFIKDIR=$DOCKERDIR/services/traefik2
 
 echo   - Create directories
-mkdir $TRAEFIKDIR/acme
-mkdir $TRAEFIKDIR/rules
-mkdir $TRAEFIKDIR/logs
+mkdir -p $TRAEFIKDIR/acme
+mkdir -p $TRAEFIKDIR/rules
+mkdir -p $TRAEFIKDIR/logs
 
-echo   - Create file `acme.json` and set permissions to 600
+echo   - Create file 'acme.json' and set permissions to 600
 touch $TRAEFIKDIR/acme/acme.json
 chmod 600 $TRAEFIKDIR/acme/acme.json
 
-echo   - Create files `access.log` and `traefik.log`
+echo   - Create files 'access.log' and 'traefik.log'
 touch $TRAEFIKDIR/logs/access.log
 touch $TRAEFIKDIR/logs/traefik.log
