353da6e9c7a9cb117ed1c694a89fbc5c7a0c950c
Michał Kluz <michalk495@wp.pl>
2023-02-07 21:49:47 +0100

    Add the feature for restarting

--- restart-service.sh
+++ restart-service.sh
@@ -3,7 +3,10 @@
 INITDIR=$(pwd)
 TARGETDIR=$(pwd)/services/$1
 
-if [ -d "$TARGETDIR" ];
+if [ "$1" = "all" ];
+then
+  for DIR in $(pwd)/services/*; do cd $DIR; docker compose up -d; docker compose down; cd $INITDIR; done
+elif [ -d "$TARGETDIR" ];
 then
   cd $TARGETDIR
   docker compose down
