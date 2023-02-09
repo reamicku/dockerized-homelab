af556bc8fdc90674ff78acfd5b0cfe381888b86e
Michał Kluz <michalk495@wp.pl>
2023-02-09 17:40:47 +0100

    Rework startup/down order

--- start.sh
+++ start.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+PROXYNAME=traefik2
 INITDIR=$(pwd)
 TARGETDIR=$(pwd)/services/$1
 
 if [ "$1" = "all" ]; then
+  # Start up the proxy first
+  cd services/$PROXYNAME
+  docker compose up -d
+  cd $INITDIR
+  # Then enable rest of the services
   for DIR in $(pwd)/services/*; do
-    cd $DIR
-    docker compose up -d
-    cd $INITDIR
+    if [ "$(basename $DIR)" != "$PROXYNAME" ]; then
+      cd $DIR
+      docker compose up -d
+      cd $INITDIR
+    fi
   done
+# Specified service does not exist
 elif [ -d "$TARGETDIR" ]; then
   cd $TARGETDIR
   docker compose up -d
